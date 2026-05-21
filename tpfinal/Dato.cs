94015182847a09c2a0198477777337e6b0359891
Axel32456 <144170744+Axel32456@users.noreply.github.com>
2026-05-21 14:11:31 -0300

    Add files via upload

--- tpfinal/Dato.cs
+++ tpfinal/Dato.cs
@@ -1,47 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace tpfinal
 {
-
-	[Serializable]
-	public class Dato
-	{
-		public int ocurrencia { get; set; }
-		public string texto { get; set; } // String of symbols
-
-		public string descripcion { get; set; } // String of symbols
-
+    public class Dato
+    {
+        public int ocurrencia { get; set; }
+        public string texto { get; set; }
+        public string descripcion { get; set; }
 
         public Dato(int ocurrencia, string texto)
         {
             this.ocurrencia = ocurrencia;
             this.texto = texto;
         }
-
         public Dato(int ocurrencia, string texto, string descripcion)
-		{
-			this.ocurrencia = ocurrencia;
-			this.texto = texto;
-			this.descripcion = descripcion;
-		}
+        {
+            this.ocurrencia = ocurrencia;
+            this.texto = texto;
+            this.descripcion = descripcion;
+        }
+        public override string ToString()
+        {
+            if (texto != null)
+            {
+                return "(" +ocurrencia +") " + texto;
+            }
 
-
-
-		public override string ToString()
-		{
-			if (texto != null)
-			{
-
-				return "(" + ocurrencia + ") " + texto;
-
-			}
-			else
-			{
-
-				return "";
-			}
-		}
-
-	}
+            else
+            {
+                return "";
+            }
+        }
+    }
 }
